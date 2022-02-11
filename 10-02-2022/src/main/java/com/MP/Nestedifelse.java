24de8e94202a8b9de1f09a94bc7003f4900da548
vaishu vollala <vaishu vollala@LAPTOP-U09TFG76>
2022-02-11 10:17:43 +0530

    commit

---
+++ 10-02-2022/src/main/java/com/MP/Nestedifelse.java
@@ -0,0 +1,22 @@
+package com.MP;
+
+public class Nestedifelse {
+	public static void main(String[] args) {
+		int age=25;
+		int weight=80;
+		if(age>=18) {
+			if(weight<=60){
+				System.out.println("Eligible to donate blood");
+			}
+			else {
+				System.out.println("Not eligible to donate blood");
+			 }}
+		else{
+			System.out.println("Age must be greater than 18");
+				
+			}
+			
+		}
+}
+
+
